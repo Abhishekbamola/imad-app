830d9d3abc07d7d6de311cb7053f14b35c73024d
Abhishekbamola <30387919+Abhishekbamola@users.noreply.github.com>
2017-08-27 00:20:18 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article one', function (req, res) {
-  res.send('article one will be served');
+  res.send('article-one will be served');
 });
 
 app.get('/article two', function (req, res) {
-  res.send('article two will be served');
+  res.send('article-two will be served');
 });
 
 app.get('/article three', function (req, res) {
-  res.send('article three will be served');
+  res.send('article-three will be served');
 });
 
 app.get('/ui/style.css', function (req, res) {
